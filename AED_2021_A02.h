86eaf10572244a34c0fa2cba52faa3b6feb06ef3
João Fonseca <joao.fonseca@ua.pt>
2022-01-15 12:33:29 +0000

    .

--- AED_2021_A02.h
+++ AED_2021_A02.h
@@ -12,8 +12,8 @@
 #define N_MAIN_INDEXES                  4 // adicionado
 #define MAX_NAME_SIZE                   31
 #define MAX_ZIP_CODE_SIZE               63
-#define MAX_TELEPHONE_NUMBER_SIZE       13
-#define MAX_SOCIAL_SECURITY_NUMBER_SIZE 12 // adicionado
+#define MAX_TELEPHONE_NUMBER_SIZE       15
+#define MAX_SOCIAL_SECURITY_NUMBER_SIZE 15 // adicionado
 
 // from random_number.c
 void aed_srandom(int seed);
