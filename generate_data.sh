48c9405ff115617954161a52078bd7bc5f833ff5
João Fonseca <joao.fonseca@ua.pt>
2022-01-16 19:02:15 +0000

    script para gerar e guardar dados

---
+++ generate_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[[ $# -ne 1 ]] && echo "Sintaxe: $0 [nMec]" && exit 1
+
+# Para cada número de pessoas
+i=4
+while [[ $i -le 10000000 ]]
+do
+    # Realizar 10 experiências
+    for e in {1..10}
+    do
+    ./multi_ordered_tree $1 $i > "./results/data$1-$i-$e.txt"
+    done
+
+    i=$((i<<1))
+done
